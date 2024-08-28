35790df644b87f47c663b1b6c567d9cdb6f479da
sidney2k1 <sidsingla02@gmail.com>
2024-08-28 17:14:10 +0530

    main

---
+++ cap.py
@@ -0,0 +1,16 @@
+a=float(input("enter your number"))
+b=float(input("enter your number"))
+c=float(input("enter your number"))
+d=float(input("enter your number"))
+sum=a+b+c+d
+print(sum)
+average=sum/4
+print(average)
+root=a**0.251345
+print(root)
+root2=b**0.2534444444444444432454224
+print(root2)
+root3=c**0.25214325454553
+print(root3)
+root4=d**0.2543437889424278427682
+print(root)
